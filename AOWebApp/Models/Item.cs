0f927420ad1654aed3b8b07da3ff67476c3016b8
Gaogang <xgg511059583@gmail.com>
2025-08-08 11:17:38 +0930

    Week 2  Model Activities:  DB 1st Model complete

--- AOWebApp/Models/Item.cs
+++ AOWebApp/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp.Models;
 
@@ -10,20 +11,28 @@ public partial class Item
     public int ItemId { get; set; }
 
     [Required(ErrorMessage = "Item name is required.")]
-    [StringLength(150, ErrorMessage = "Item name cannot exceed 150 characters.")]
+    [Display(Name = "Item Name")]
+    [StringLength(150, ErrorMessage = "Item name should less than 150 characters.")]
     public string ItemName { get; set; } = null!;
 
+
     [Required(ErrorMessage = "Description is required.")]
+    [Display(Name = "Item Description")]
     public string ItemDescription { get; set; } = null!;
 
     [Required(ErrorMessage = "Cost is required.")]
     [Range(1,10000)]
-    [DataType(DataType.Currency)]          
-    //[Column(TypeName = "decimal(18, 2)")]
+    [DataType(DataType.Currency)]
+    [Display(Name = "Item Cost")]
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal ItemCost { get; set; }
 
+    [Required(ErrorMessage = "Image URL is required.")]
+    [Display(Name = "Item Image URL")]
+    [DataType(DataType.ImageUrl)]
     public string ItemImage { get; set; } = null!;
 
+    [Required(ErrorMessage = "Category ID is required.")]
     public int CategoryId { get; set; }
 
     public virtual ItemCategory Category { get; set; } = null!;
