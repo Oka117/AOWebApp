fa4fde5864e1681ef381527c160d7eaed25096b0
Gaogang <xgg511059583@gmail.com>
2025-10-27 23:53:50 +1030

    Week 11 Practical Complete

--- AOWebApp/aowebapp/aowebapp/src/components/CardList.js
+++ AOWebApp/aowebapp/aowebapp/src/components/CardList.js
@@ -1,10 +1,40 @@
-﻿import Card from "./CardV3"
-import cardData from "../assets/itemData.json"
+﻿import React, { useState, useEffect } from 'react'
+import Card from "./CardV3"
+//import cardData from "../assets/itemData.json"
 
-const CardList = ({ }) => {
-    console.log("cardData: " + cardData);
+
+const CardListSearch = ({ }) => {
+    const [cardData, setState] = useState([]);
+    const [query, setQuery] = useState('');
+
+    React.useEffect(() => {
+        console.log("useEffect");
+        fetch(`http://localhost:5180/api/itemsWebAPI?searchText=${query}`)
+            .then(response => response.json())
+            .then(data => setState(data))
+            .catch(err => {
+                console.log(err);
+            });
+    }, [query])
+
+    function searchQuery(evt) {
+        const value = document.querySelector('[name="searchText"]').value;
+        alert('value: ' + value);
+        setQuery(value);
+    }
+
+    //console.log("cardData: ", cardData);
     return (
-        <div className="row">
+        <div id="cardListSearch">
+          <div className="row justify-content-start mb-3">
+            <div className="col-3">
+                <input type="text" name="searchText" className="form-control" placeholder="Type your query" />
+            </div>
+            <div className="col text-left">
+                <button type="button" className="btn btn-primary" onClick={searchQuery}>Search</button>
+            </div>
+        </div>
+        <div id = "cardList" className="row">
             {cardData.map((obj) => (
                 <Card
                     key={obj.itemId}
@@ -15,8 +45,9 @@ const CardList = ({ }) => {
                     itemImage={obj.itemImage}
                 />
             ))}
+            </div>
         </div>
     )
 }
 
-export default CardList
+export default CardListSearch
